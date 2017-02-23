9e02ce0ab827783ec1a75d4fdce85c6505b53f1c
Daniel Molin <dkd333@gmail.com>
2017-02-23 16:41:54 +0100

    moqups: Close fullscreen on Ctrl+F; code refactor

--- moqups-full-preview.user.js
+++ moqups-full-preview.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Moqups Full Preview
 // @namespace    dkdscripts
-// @version      1.0
+// @version      1.1
 // @description  Press Ctrl+F11 to hide all menus and go FULL PREVIEW
 // @author       dkd
 // @downloadURL  https://github.com/mold/userscripts/raw/master/moqups-full-preview.user.js
@@ -32,43 +32,55 @@
 		};
 	}
 
-	window.addEventListener("keydown", function(evt) {
+	function toggleFullscreen(full) {
 		if (debounce) {
 			return;
 		}
 
-		if (evt.keyCode === 122 && evt.ctrlKey) {
-			if (barsHidden) {
-				mainStage.css(mainStage.origCSS);
-				stageWrapper.css(stageWrapper.origCSS);
-			} else {
-				getElements();
-
-				mainStage.css({
-					left: "0",
-					right: 0,
-					"z-index": "1000",
-					top: 0,
-					bottom: 0,
-					transition: transitionDuration + "ms all",
-
-				});
-
-				stageWrapper.css({
-					"z-index": "1000",
-					transition: transitionDuration + "ms all",
-					top: 0,
-				})
-			}
-
-			barsHidden = !barsHidden;
-			debounce = true;
+		if (undefined === full) {
+			full = !barsHidden;
 		}
 
+		if (full) {
+			getElements();
+
+			mainStage.css({
+				left: "0",
+				right: 0,
+				"z-index": "1000",
+				top: 0,
+				bottom: 0,
+				transition: transitionDuration + "ms all",
+
+			});
+
+			stageWrapper.css({
+				"z-index": "1000",
+				transition: transitionDuration + "ms all",
+				top: 0,
+			});
+		} else {
+			mainStage.css(mainStage.origCSS);
+			stageWrapper.css(stageWrapper.origCSS);
+		}
+
+		barsHidden = !barsHidden;
+		debounce = true;
 
 		window.setTimeout(function() {
 			debounce = false;
-		}, transitionDuration)
+		}, transitionDuration);
+	}
+
+	window.addEventListener("keydown", function(evt) {
+		// Ctrl+F11
+		if (evt.keyCode === 122 && evt.ctrlKey) {
+			toggleFullscreen();
+		}
+		// Ctrl+F
+		else if (evt.keyCode === 70 && evt.ctrlKey) {
+			toggleFullscreen(false);
+		}
 	});
 
 })();
